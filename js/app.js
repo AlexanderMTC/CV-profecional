339e4b4dc3da6b473524c49e772cf58bb5d31027
alexander.mtc <alexander.mtc@gmail.com>
2023-06-08 18:56:35 -0300

    Modif CSS cambio a BEM

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector(".menu");
-const openMenuBtn = document.querySelector(".open-menu");
-const closeMenuBtn = document.querySelector(".close-menu");
+const openMenuBtn = document.querySelector(".topnav__menu--open");
+const closeMenuBtn = document.querySelector(".topnav__menu--close");
+console.log(openMenuBtn, closeMenuBtn);
 
 function toggleMenu() {
   menu.classList.toggle("menu_opened");
@@ -10,6 +11,6 @@ openMenuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
 
 document.addEventListener("click", (e) => {
-  if (!e.target.matches(".menuLink")) return false;
+  if (!e.target.matches(".menu__Link")) return false;
   menu.classList.remove("menu_opened");
 });
